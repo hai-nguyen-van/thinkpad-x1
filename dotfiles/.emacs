b18618e4a876ef9d124f467d75302419a566345c
Hai Nguyen-Van <hai.nguyen-van@lri.fr>
2015-02-14 22:43:24 +0200

    minor changes

--- dotfiles/.emacs
+++ dotfiles/.emacs
@@ -222,6 +222,6 @@
 
 ;; Stop Emacs from “contaminating” the clipboard when 
 ;; Ensures the system clipboard has the latest content copied *outsite* Emacs
-(setq x-select-enable-clipboard nil)
+; (setq x-select-enable-clipboard nil)
 ;; Paste shortcut for system clipboard (instead of kill ring)
-(global-set-key (kbd "C-c C-v") 'x-clipboard-yank)
+; (global-set-key (kbd "C-c C-v") 'x-clipboard-yank)
--- next_ratp_bus.sh
+++ next_ratp_bus.sh
@@ -4,7 +4,7 @@
 # Hai Nguyen Van <nguyenva@informatique.univ-paris-diderot.fr>
 
 # Usage: ./next_ratp_bus.sh [-a]
-# -a : Print the result on the libnotify/notify-osd notification service
+# -a : Prints the result on the libnotify/notify-osd notification service
 
 # To change the configuration of this app, you need to retrieve RATP_URL by entering your desired location in the following link then get back the new link
 # http://www.ratp.fr/horaires/fr/ratp/bus
@@ -16,7 +16,7 @@ RATP_STOP_NAME="Div. Leclerc Camille Desmoulins"
 RATP_DIRECTION="Porte d'Orleans"
 RATP_URL="http://www.ratp.fr/horaires/fr/ratp/bus/prochains_passages/PP/B187/187_598_601/R"
 
-_ALERT_IMG_LOC_="/home/psaxl/code/scripts/images/ratp.png"
+_ALERT_IMG_LOC_="$(pwd)/img/ratp.png"
 
 # testing internet connection
 if (! (ping -c 1 google.com > /dev/null 2> /dev/null))
@@ -31,7 +31,7 @@ TIME2=$(echo ${_WEB_RES_} | cut -d ' ' -f 2)
 
 if [ $# = 1 ]
 then
-    DISPLAY=:0.0 XAUTHORITY=~/.Xauthority notify-send -u critical -i "${_ALERT_IMG_LOC_}" "Next stops in ${TIME1} min and ${TIME2} min at ${RATP_STOP_NAME}"
+    DISPLAY=:0.0 XAUTHORITY=~/.Xauthority notify-send -u critical -i "${_ALERT_IMG_LOC_}" "Next RATP Bus Arrivals" "Route: B${RATP_BUS_NO}\nDirection: ${RATP_DIRECTION}\nStop: ${RATP_STOP_NAME}\nArriving ${TIME1} min and ${TIME2} min"
 else
     echo "Next stops in ${TIME1} min and ${TIME2} min at ${RATP_STOP_NAME}"
 fi
